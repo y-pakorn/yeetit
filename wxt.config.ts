aab7d574570a60ef3382b1a231a49d4e11aef8f3
Pakorn Nathong <pakorn.nth@hotmail.com>
2025-09-18 13:49:13 +0700

    fix: add 'tabs' permission to wxt.config.ts for enhanced functionality

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    permissions: ["activeTab", "sidePanel", "storage"],
+    permissions: ["activeTab", "sidePanel", "storage", "tabs"],
     action: {
       default_title: "YeetIt Sidepanel",
     },
