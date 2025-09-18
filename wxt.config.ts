f42fc1a08db6eab836cca644cd29027a856fc2cf
Pakorn Nathong <pakorn.nth@hotmail.com>
2025-09-18 13:23:03 +0700

    fix: update permissions in wxt.config.ts to remove unused 'scripting' and 'tabs'

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    permissions: ["activeTab", "scripting", "sidePanel", "storage", "tabs"],
+    permissions: ["activeTab", "sidePanel", "storage"],
     action: {},
     name: "YeetIt",
     description: "YeetIt",
